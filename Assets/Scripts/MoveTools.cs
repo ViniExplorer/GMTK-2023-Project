95afd98bddeb63644e2392be667a95b16eaf678d
ViniExplorer <viniciusbianco2006@gmail.com>
2023-06-22 15:28:35 +0100

    Initial commit of project assets.

---
+++ Assets/Scripts/MoveTools.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MoveTools
+{
+    public static IEnumerator SmoothMovement(GameObject self, Vector3 targetPos, float finalDistance, float moveTime)
+    {
+        while(Vector2.Distance(self.transform.position, targetPos) > finalDistance)
+        {
+            Debug.Log("Walking");
+            Vector2 step = Vector2.MoveTowards(self.transform.position, targetPos, moveTime * Time.deltaTime);
+
+            self.GetComponent<Rigidbody2D>().MovePosition(step);
+
+            yield return null;
+        }
+        Debug.Log("Walked");
+    }
+}
